ee2c7b3ddc188c17a54c01ece2bde76a36888388
Martin Hohenberg <me@martinhohenberg.de>
2020-11-07 09:35:49 +0000

    A lot more statistics

--- statistics.php
+++ statistics.php
@@ -1,8 +1,17 @@
+<html>
+<head>
+	<title>Internal Statistics / Alpaca Bingo</title>
+</head>
+<body>
+<header>
+<a href='#terms'>Bingo Terms</a> 
+<a href='#sessions'>Sessions</a>
+</header>
 <?php
 
 $keywords = explode("\n",file_get_contents("keywords.org"));
 
-$lines = [];
+$terms = [];
 
 foreach ($keywords as $keyword) {
 	$kwMD5 = md5($keyword);
@@ -14,24 +23,61 @@ foreach ($keywords as $keyword) {
 		$count = file_get_contents($filename);
 	}
 
-	array_push($lines, "<td>".$count."</td><td>$kwMD5</td><td>".$keyword."</td>");
+	if ($count < 10) {
+		$count = "0".$count;
+	}
+
+	array_push($terms, "<td>".$count."</td><td>".$keyword."</td>");
 }
 
-rsort($lines);
-$linespertier = count($lines)/3;
+rsort($terms);
+$termsPerTier = count($terms)/3;
 
-echo "Lines per tier: $linespertier";
-
+echo "<h1 id='terms'>Bingo Terms</h1>";
 echo "<table style='width:80%;margin:auto'>";
-foreach ($lines as $line) {
+echo "<tr><th>Times claimed</th><th>Bingo card</th></tr>";
+foreach ($terms as $term) {
 	$i++;
-	if ($i < $linespertier) {
+	if ($i < $termsPerTier) {
 		$backgroundcolor = "#dfd";
-	} elseif ($i < $linespertier*2) {
+	} elseif ($i < $termsPerTier*2) {
 		$backgroundcolor = "#dff";
 	} else {
 		$backgroundcolor = "#fdd";
 	}
-	echo "<tr style='background-color:$backgroundcolor'>$line</tr>";
+	echo "<tr style='background-color:$backgroundcolor'>$term</tr>";
 }
+
 echo "</table>";
+////////////////////////////
+
+$sessionsNum = 0;
+$filledCards = 0;
+$bestFilled = 0;
+$files = glob("sessions/*");
+foreach($files as $file) {
+	$sessionsNum++;
+	$sessionCompletion = 0;
+	$sessionclickFiles = glob("sessionclick/".str_replace("sessions/","",$file)."*");
+	foreach ($sessionclickFiles as $hf) {
+		$filledCards++;
+		$sessionCompletion++;
+	}
+	if ($bestFilled < $sessionCompletion) {
+		$bestFilled = $sessionCompletion;
+	}
+}
+
+$sessionCompletion = $filledCards." (".round($filledCards/$sessionsNum,2)." per session // best filled: ".$bestFilled.")";
+
+echo "<h1 id='session'>Sessions</h1>";
+echo "<table>";
+
+echo "<tr><th>Currently active sessions/players</th><td>$sessionsNum</td></tr>";
+echo "<tr><th>Search Terms per session</th><td>$sessionCompletion</td></tr>";
+
+echo "</table>";
+
+?>
+</body>
+</html>
